2522889abde255d349b9487263241165e749c13c
vneto3646-beep <vneto3646@gmail.com>
2026-04-15 11:39:16 -0300

    Update mecanica.py

--- mecanica.py
+++ mecanica.py
@@ -1,4 +1,5 @@
 from datetime import date
+import time
 
 class Pessoa: 
     def __init__(self, nome, telefone, data_nascimento):
@@ -7,12 +8,12 @@ class Pessoa:
         self.data_nascimento = data_nascimento
 
 def Registro_Pessoa(self):
-    obj1 = Pessoa("Jamille Cristiny Kurek Ferreira", "47 984831723", date(2009, 12, 09))
-    obj2 = Pessoa("Manuella Sperotto", "47 981267434", date(2010, 02, 11))
-    obj3 = Pessoa("Vicente Neto Fernandes Menezes", "47 908714364", date(2009, 03, 21))
-    obj4 = Pessoa("Fabricio", "47 984010893", date(1989, 04, 02))
-    obj5 = Pessoa("Luana", "47 998612794", date(1986, 12, 04))
-    obj6 = Pessoa("Marcos", "47 998613141", date(1970, 08, 01))
+    obj1 = Pessoa("Jamille Cristiny Kurek Ferreira", "47 984831723", date(2009, 12, 9))
+    obj2 = Pessoa("Manuella Sperotto", "47 981267434", date(2010, 2, 11))
+    obj3 = Pessoa("Vicente Neto Fernandes Menezes", "47 908714364", date(2009, 3, 21))
+    obj4 = Pessoa("Fabricio", "47 984010893", date(1989, 4, 2))
+    obj5 = Pessoa("Luana", "47 998612794", date(1986, 12, 4))
+    obj6 = Pessoa("Marcos", "47 998613141", date(1970, 8, 1))
 
 class Cliente: 
     def __init__ (self, pessoa, email):
@@ -25,7 +26,7 @@ def Registro_Cliente(self):
     obj3 = Cliente("Vicente Neto Fernandes Menezes", "vicente@gmail.com")
 
 class Veiculo:
-    def __init__ (self, tipo, placa, cor, tanque, rendimento, odometro, desenho):
+    def __init__ (self, tipo, placa, cor, tanque, rendimento, desenho):
         self.tipo = tipo
         self.placa = placa
         self.cor = cor
@@ -35,11 +36,11 @@ class Veiculo:
         self.desenho = desenho
     def andar(self):
         self.odometro += self.rendimento    
-        self.tanque -= 1 # self.tanque = self.tanque - 1
+        self.tanque -= 1
 
 def Registro_Veiculo(self):
-    obj1 = Veiculo("carro", "bra2e19", "vermelho")
-    obj2 = Veiculo("onibus", "hda5q94", "amarelo")
+    obj1 = Veiculo("carro", "bra2e19", "vermelho", 40, 11, "o-o")
+    obj2 = Veiculo("onibus", "hda5q94", "amarelo", 80, 8, "o~W~o")
 
 def __str__(self):
     return "Placa:"+self.placa+",cor:"+self.cor
@@ -50,8 +51,8 @@ class VeiculoComPassageiro(Veiculo):
         super().__init__(tipo, placa, cor)
 
 def Registro_VeiculoComPassageiro(self):
-    obj1 = VeiculoComPassageiro("carro", "bra2e19", "vermelho", "5")
-    obj2 = VeiculoComPassageiro("onibus", "hda5q94", "amarelo", "40")
+    obj1 = VeiculoComPassageiro("carro", "bra2e19", "vermelho", 5)
+    obj2 = VeiculoComPassageiro("onibus", "hda5q94", "amarelo", 40)
 
 def __str__(self):
     return f"Lugares:{self.lugares}{super().__str__()}"
@@ -120,38 +121,36 @@ class OrdemServico:
         self.desconto = desconto
 
 def Registro_OrdemServico(self):
-    obj1 = OrdemServico(date(2026, 08, 04), "bra2e19", date(2026, 10, 04), "Jamille Cristiny Kurek Ferreira", 0)
-    obj2 = OrdemServico(date(2026, 01, 19), "hda5q94", date(2026, 01, 21), "Manuella Sperotto", 0)
-    obj3 = OrdemServico(date(2026, 04, 22), "plq0j13", date(2026, 04, 23), "Vicente Neto Fernandes Menezes", 0)
+    obj1 = OrdemServico(date(2026, 8, 4), "bra2e19", date(2026, 10, 4), "Jamille Cristiny Kurek Ferreira", 0)
+    obj2 = OrdemServico(date(2026, 1, 19), "hda5q94", date(2026, 1, 21), "Manuella Sperotto", 0)
+    obj3 = OrdemServico(date(2026, 4, 22), "plq0j13", date(2026, 4, 23), "Vicente Neto Fernandes Menezes", 0)
 
 class Pista:
-    def __init__(self, extensao, veiculos):
+    def __init__(self, extensao):
         self.extensao = extensao
         self.veiculos = []
 
     def tick(self):
         for v in self.veiculos:
             if v.odometro <= self.extensao:
-                v.andar() # anda, veículo :-)
+                v.andar() 
 
     def pista_grafica(self):
         for v in self.veiculos:
             s = "." * int(v.odometro/10)
-            print(v.modelo,"(",v.odometro,"):",s,v.desenho)            
+            print(v.tipo,"(",v.odometro,"):",s,v.desenho)            
 
-fox = Veiculo("Fox", 42, 12, "o~W~o")
-biz = Veiculo("Honda Biz", 5, 40, "o-o")
+Uno = Veiculo("carro", "bra2e19", "vermelho", 42, 12, "o-o")
+Volkmann = Veiculo("onibus", "hda5q94", "amarelo", 5, 40, "o~W~o")
 
 indianapolis = Pista (500)
-indianapolis.veiculos.append(fox)
-indianapolis.veiculos.append(biz)
+indianapolis.veiculos.append(Uno)
+indianapolis.veiculos.append(Volkmann)
 while True:
     indianapolis.tick() 
-    #print(indianapolis.veiculos[0].modelo, "andou", indianapolis.veiculos[0].odometro)
-    #print(indianapolis.veiculos[1].modelo, "andou", indianapolis.veiculos[1].odometro)
     indianapolis.pista_grafica()
     
-    time.sleep(1)
+    time.sleep(0.30)
 
     if indianapolis.veiculos[0].odometro >= 500 and indianapolis.veiculos[1].odometro >= 500:
         break 
